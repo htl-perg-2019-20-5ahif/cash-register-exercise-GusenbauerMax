48058236e4fa84c267472f1ec5f69279362bc186
Max Gusenbauer <max.gusenb@gmail.com>
2020-02-12 22:04:18 +0100

    Added Solution

---
+++ CashRegister/CashRegister.WebApi/Data/CashRegisterDataContext.cs
@@ -0,0 +1,38 @@
+﻿using CashRegister.Shared;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CashRegister.WebApi.Data
+{
+    public class CashRegisterDataContext : DbContext
+    {
+        public CashRegisterDataContext(DbContextOptions<CashRegisterDataContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ReceiptLine> ReceiptLines { get; set; }
+        public DbSet<Receipt> Receipts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().Property(p => p.ProductName).IsRequired();
+            modelBuilder.Entity<Product>().Property(p => p.UnitPrice).IsRequired();
+
+            modelBuilder.Entity<ReceiptLine>().Property(rl => rl.Amount).IsRequired();
+            modelBuilder.Entity<ReceiptLine>().Property(rl => rl.TotalPrice).IsRequired();
+
+            modelBuilder.Entity<Receipt>().Property(r => r.ReceiptTimestamp).IsRequired();
+            modelBuilder.Entity<Receipt>().Property(r => r.TotalPrice).IsRequired();
+
+            modelBuilder.Entity<ReceiptLine>()
+                .HasOne(rl => rl.Product);
+
+            modelBuilder.Entity<Receipt>()
+                .HasMany(r => r.ReceiptLines);
+        }
+    }
+}
